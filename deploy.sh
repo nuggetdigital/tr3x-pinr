264f38b08a11662868d294347bb38cc508f6143f
chiefbiiko <noah.anabiik.schwarz@gmail.com>
2021-05-12 17:17:16 +0200

    fix: a hardcoded cfn stack name

--- deploy.sh
+++ deploy.sh
@@ -52,7 +52,7 @@ if [[ "$instance_replacement" == "True" ]]; then
   )"
 
   volume_id="$( \
-    jq -r '.Stacks[] | select(.StackName == "bluford") | .Outputs[] | select(.OutputKey == "VolumeId") | .OutputValue' <<< "$stack" \
+    jq -r ".Stacks[] | select(.StackName == \"$STACK_NAME\") | .Outputs[] | select(.OutputKey == \"VolumeId\") | .OutputValue" <<< "$stack" \
   )"
 
   instance_id="$(jq -r '.ResourceChange.PhysicalResourceId' <<< "$instance")"
