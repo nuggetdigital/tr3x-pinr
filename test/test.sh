53bf1e46cf7f34efd479a47845c13f8819fd2b03
chiefbiiko <noah.anabiik.schwarz@gmail.com>
2021-05-20 15:11:56 +0200

    test: suite consolidated

--- test/test.sh
+++ test/test.sh
@@ -1,6 +1,5 @@
 source <(curl -sSf https://raw.githubusercontent.com/chiefbiiko/bashert/v1.1.0/bashert.sh)
-
-source ./.env
+source ../.env
 
 stacks="$(aws cloudformation describe-stacks --stack-name $STACK_NAME)"
 alb="$( \
@@ -16,7 +15,7 @@ test_add_a_file_200() {
   curl \
     -F "file=@./celesta.wav" \
     -D "$resp_head" \
-    -vL# \
+    -L# \
     "http://$alb/api/v0/add?cid-version=1&hash=blake2b-256&pin=false" \
   > $resp_body
 
@@ -25,26 +24,25 @@ test_add_a_file_200() {
   cid=$(jq -r '.Hash' $resp_body)
 
   assert_match "$cid" '^[a-z2-7]+=*$'
-
   assert_equal ${#cid} 62
-
   assert_equal "$cid" $(<./celesta.cid) 
 }
 
 test_get_a_file_200() {
   printf "test_get_a_file_200\n"
-
-  printf "NOTE: test_get_a_file_200 needs to run AFTER test_add_a_file_200\n"
+  printf "NOTE: needs to run AFTER test_add_a_file_200\n"
 
   resp_head=$(mktemp)
   resp_body=$(mktemp)
+  cid=$(<./celesta.cid)
 
   curl \
     -X POST \
     -D $resp_head \
-    http://$alb/api/v0/cat?arg=$(<./celesta.cid) \
+    -L# \
+    http://$alb/api/v0/cat?arg=$cid \
   > $resp_body
-  echo "[DBG]" && cat $resp_head && cat $resp_body && echo "[DBGEND]"
+
   assert_status $resp_head 200
 
   assert_files_equal $resp_body ./celesta.wav
