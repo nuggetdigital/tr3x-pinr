098e8445a031e4dbc77ada1bc4bc05947b642712
chiefbiiko <noah.anabiik.schwarz@gmail.com>
2021-06-08 14:18:28 +0200

    test: switch the integration tests to da nu api

--- test/test.sh
+++ test/test.sh
@@ -17,44 +17,35 @@ test_add_a_file_200() {
 
   curl \
     -F "file=@$wd/celesta.wav" \
-    -D "$resp_head" \
+    -D $resp_head \
     -vL# \
-    "http://$alb/api/v0/add?cid-version=1&hash=blake2b-256&pin=false" \
+    http://$alb/ \
   > $resp_body
 
   assert_status $resp_head 200
 
   cid=$(jq -r '.Hash' $resp_body)
 
-  assert_match "$cid" '^[a-z2-7]+=*$'
+  assert_match $cid '^[a-z2-7]+=*$'
   assert_equal ${#cid} 62
-  assert_equal "$cid" $(<$wd/celesta.cid) 
+  assert_equal $cid $(<$wd/celesta.cid) 
 }
 
+# NOTE: needs to run AFTER test_add_a_file_200
 test_get_a_file_200() {
   printf "test_get_a_file_200\n"
-  printf "NOTE: needs to run AFTER test_add_a_file_200\n"
 
   resp_head=$(mktemp)
   resp_body=$(mktemp)
   cid=$(<$wd/celesta.cid)
 
   curl \
-    -X POST \
     -D $resp_head \
     -vL# \
-    http://$alb/api/v0/cat?arg=$cid \
+    http://$alb/$cid \
   > $resp_body
 
   assert_status $resp_head 200
 
   assert_files_equal $resp_body $wd/celesta.wav
-}
-
-test_add_a_file_twice_200() {
-  printf "NOT IMPLEMENTED: test_add_a_file_twice_200\n"
-}
-
-test_api_shield() {
-   printf "NOT IMPLEMENTED: test_api_shield\n"
 }
